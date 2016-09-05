6111cb8d65a87437510f1af23a9d47bc02390e05
Thomas Coopman <thomas.coopman@protime.eu>
2016-09-05 10:17:44 +0200

    c# testing

---
+++ dotnet/bowling_game.console_app/BowlingGameTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bowling_game.console_app
+{
+    [TestFixture]
+    public class BowlingGameTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
